c1b7009c047e45e136599d7f6f116b8b62735f13
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-16 12:52:25 -0800

    Script to print using printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print using 
+ * printf and having a new line at 
+ * the end
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
