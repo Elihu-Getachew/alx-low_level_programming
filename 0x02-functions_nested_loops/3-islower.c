91e31137a25fa0d3daf9953747654dc50523577e
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-21 08:11:02 -0800

    Fixed betty errors

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,16 @@
 /**
  * _islower - checks if a certain character is
  * given in lower case or not
- * int c - the function _islower takes a
- * parameter of type int
+ *
+ * @c: the function _islower takes a parameter
+ * of type int which it then uses in its comparison
  *
  * Return: 1 for lower case
  * 0 for reults otherwise
  */
 int _islower(int c)
 {
-	if (97<= c && c <=122)
+	if (97 <= c && 122 >= c)
 		return (1);
 	else
 		return (0);
