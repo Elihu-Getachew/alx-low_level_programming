49c6b6d89bcbf19f12256f83f66c0fa702aca097
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-21 09:33:28 -0800

    Addition task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds 2 numbers and returns the result
+ *
+ * @n: number 1
+ *
+ * @i: number 2
+ *
+ * Return: returns the addition result
+ */
+int add(int n, int i)
+{
+	return (n + i);
+}
