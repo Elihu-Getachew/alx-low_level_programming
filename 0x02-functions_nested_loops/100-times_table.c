c6cdd12373c5acd89ab320c51514585813efdc90
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-21 11:40:31 -0800

    Print times table for numbers between 0 and 15 task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints a times table starting from 0
+ * to a certain number specified by the input n, if
+ * the number n is between 0 and 15
+ *
+ * @n: is the number taken as an input
+ *
+ * Return: no return
+ */
+void print_times_table(int n)
+{
+	int a, b, mul;
+
+	if (n <= 0 && n >= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
+				mul = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (mul <= n)
+				{
+					_putchar(32);
+					_putchar(mul + 48);
+				}
+				else
+				{
+					_putchar((mul / 10) + 48);
+					_putchar((mul % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
