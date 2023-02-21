d6e65914a1c75d36b53b202b86af5360ba973915
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-21 08:01:35 -0800

    Islower task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,17 @@
 /**
  * _islower - checks if a certain character is
  * given in lower case or not
+ * int c - the function _islower takes a
+ * parameter of type int
  *
  * Return: 1 for lower case
  * 0 for reults otherwise
  */
 int _islower(int c)
 {
-	if (97<= c || c <=122)
+	char var = (char)(c);
+
+	if (97<= var || var <=122)
 		return (1);
 	else
 		return (0);
