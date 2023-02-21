8250d7ad322a747649e14fb35d3abdf5d66ffb36
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-21 11:59:57 -0800

    Fixed logical error on 100-times_table with if else statement

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,10 +26,12 @@ void print_times_table(int n)
 				if (mul <= 9)
 				{
 					_putchar(32);
+					_putchar(32);
 					_putchar(mul + 48);
 				}
 				else if (mul <= 99)
 				{
+					_putchar(32);
 					_putchar((mul / 10) + 48);
 					_putchar((mul % 10) + 48);
 				}
