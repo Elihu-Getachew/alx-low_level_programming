8f0ff7352aef2592496fe57b19e00ee3247c7089
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-21 07:19:05 -0800

    Print the alphabet 10 times task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet from
+ * a to z 10 times
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
