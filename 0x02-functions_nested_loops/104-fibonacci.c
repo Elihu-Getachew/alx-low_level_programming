3dcbbf665bd2072958193cb3af0632e117ab64ce
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-22 10:50:54 -0800

    Fibonacci sequence for first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 100 numbers of the Fibonacci sequence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	unsigned long int a, b, fib;
+
+	a = 1;
+	b = 2;
+	printf("%lu, %lu", a, b);
+	for (n = 0; n < 96; n++)
+	{
+		fib = a + b;
+		printf(", %lu", fib);
+		a = b;
+		b = fib;
+	}
+	printf("\n");
+
+	return (0);
+}
