e1082582c13d573c146845c09dabf8c7ff9bb884
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-24 10:55:31 -0800

    Print numbers using putchar task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_number - this code prints numbers using the _putchar function that only prints one character at a time.
+ * this is done first by checking the sign and printing a '-' before negative numbers. Then what is done is setting
+ * a count variable used to check if a number is in the tenth, hundredth, thousandth ... etc pile and handle it accordingly.
+ *
+ * @n: the number to be printed
+ *
+ * Return: there is no return here it's void
+ */
+void print_number(int n)
+{
+	unsigned int i, j, count;
+
+	if (n < 0)
+	{
+		n *= -1;
+		i = n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + '0');
+	}
+}
