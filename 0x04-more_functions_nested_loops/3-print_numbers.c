856c5b1085893a75654d5a4184b0d4aabb211236
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-24 01:57:56 -0800

    Print numbers from 0 to 9 task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - this is a function that prints
+ * numbers starting from 0 to 9, followed by a new line
+ *
+ * Return: no return type or it is void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
