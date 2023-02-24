ee7db7bc3bf7aa142bfbf3ba762e7b96d8b5e7ba
Elihu-Getachew <getachewelihu@gmail.com>
2023-02-24 07:08:53 -0800

    Print triangles task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_triangle - this is a function that prints a triangle by
+ * printing '#' horizontally in a right aligned manner and on size
+ * number of lines vertically
+ *
+ * @size: is a parameter that defines the size of the triangle
+ *
+ * Return: no return type or it is void
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+
+	while (i <= size)
+	{
+		int j = 0;
+
+		while (j <= (size - i))
+		{
+			_putchar(' ');
+			j++;
+		}
+
+		j = 0;
+
+		while (j <= (i - 1))
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
