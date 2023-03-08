bdc10b1f51e9a4a748d438a0ee9a230db2433d11
Elihu-Getachew <getachewelihu@gmail.com>
2023-03-08 12:12:37 -0800

    Puts recursion task

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - defines the putchar function
+ *
+ * Return: success code 1 incase of a
+ * successful write
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));	
+}
